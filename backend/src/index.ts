b7c9a691fa2fb156409b0d7c7ffb4987f8cfeda9
Bank App Developer <student@auca.ac.rw>
2026-03-01 10:20:17 +0200

    Add explicit OPTIONS handler

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -9,15 +9,19 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // CORS must be first
+app.options('*', (req, res) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.sendStatus(204);
+});
+
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.header('Access-Control-Allow-Credentials', 'true');
-  
-  if (req.method === 'OPTIONS') {
-    return res.sendStatus(200);
-  }
   next();
 });
 
